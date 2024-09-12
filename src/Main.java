8929d239b3f397db51e1c300435c1a00bcea3483
AlexanderK <kriss.sacha@gmail.com>
2024-09-12 14:15:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+
+        while (a > 0){
+            a++;
+        }
+        System.out.println(a-1);
+    }
+}
